e213f292773a1ced942d67b0e60dcc86e0f982d1
Yonanni <adhanamengisteab@gmail.com>
2022-01-27 00:22:18 +0100

    Update w2-homework.sql

--- Database/W-02/w2-homework.sql
+++ Database/W-02/w2-homework.sql
@@ -1,33 +1,33 @@
--- 1. 
-prepare capital_city from 'select city.name from city inner join country on country.capital = city.id where country.name = ?';
-set @input = 'Eritrea';
-execute capital_city using @input;
-deallocate prepare capital_city;
+-- 1. What is the capital of country X ? (Accept X from user)
+PREPARE capital_city FROM 'SELECT city.name FROM city INNER JOIN country ON country.capital = city.id WHERE country.name = ?';
+SET @a = 'Eritrea';
+EXECUTE capital_city USING @a;
+DEALLOCATE PREPARE capital_city;
 
 -- 2. 
-prepare regional_languages from 'select distinct language from countrylanguage inner join country on country.code = countrylanguage.countrycode where country.region = ?';
-set @input = 'Eastern Africa';
-execute regional_languages using @input;
-deallocate prepare regional_languages;
+PREPARE regional_languages FROM 'SELECT DISTINCT language FROM countrylanguage INNER JOIN country ON country.code = countrylanguage.countrycode WHERE country.region = ?';
+SET @a = 'Eastern Africa';
+EXECUTE regional_languages using @a;
+DEALLOCATE PREPARE regional_languages;
 
 -- 3.
-prepare num_of_cities_theLanguage_spoken from 'select count(city.name) as num_of_cities from city inner join countrylanguage on city.countrycode = countrylanguage.countrycode where language = ?';
-set @input = "Afar";
-execute num_of_cities_theLanguage_spoken using @input;
-deallocate prepare num_of_cities_theLanguage_spoken;
+PREPARE num_of_cities_theLanguage_spoken FROM 'SELECT COUNT(city.Name) as num_of_cities from city inner join countrylanguage on city.countrycode = countrylanguage.countrycode WHERE language = ?';
+set @a = "Afar";
+EXECUTE num_of_cities_theLanguage_spoken USING @a;
+DEALLOCATE PREPARE num_of_cities_theLanguage_spoken;
 
 -- 4. 
-prepare num_of_languages_in_continent from 'select distinct continent, count(distinct language) as num_of_languages from country inner join countrylanguage on country.code = countrylanguage.countrycode group by continent';
-execute num_of_languages_in_continent;
-deallocate prepare num_of_languages_in_continent;
+PREPARE num_of_languages_in_continent FROM 'SELECT DISTINCT continent, COUNT(DISTINCT language) as num_of_languages FROM country INNER JOIN countrylanguage ON country.code = countrylanguage.countrycode GROUP BY continent';
+EXECUTE num_of_languages_in_continent;
+DEALLOCATE PREPARE num_of_languages_in_continent;
 
 -- 5. 
-select distinct name from country 
-inner join countrylanguage on country.code = countrylanguage.countrycode 
-where countrylanguage.language IN 
-(select language from countrylanguage 
-inner join country on countrylanguage.countrycode = country.code 
-where country.name = "France" and isofficial = "T") 
-and country.continent = (select country.continent from country where country.name = "France") 
-and isofficial = "T";
+SELECT DISTINCT Name FROM country 
+INNER JOIN countrylanguage ON country.code = countrylanguage.countrycode 
+WHERE countrylanguage.language IN 
+(SELECT language FROM countrylanguage 
+INNER JOIN country ON countrylanguage.countrycode = country.code 
+WHERE country.name = "France" AND isofficial = "T") 
+AND country.continent = (SELECT country.continent FROM country WHERE country.name = "France") 
+AND isofficial = "T";
 
